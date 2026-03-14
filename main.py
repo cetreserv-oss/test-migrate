786eb7f8abb410c82c6605618270a1ba68609f96
Ilya Sviridov <ilya.sviridov@yandex.ru>
2026-03-14 10:15:30 +0300

    Update greeting

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-﻿def main():\n    print('Hello from sync test')\n\nif __name__ == '__main__':\n    main()\n
+﻿def main():\n    print('Hello from sync test v2')\n\nif __name__ == '__main__':\n    main()\n
